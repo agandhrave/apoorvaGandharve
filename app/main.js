2b1daa7c544fa2abba2e67a0cd179739d0705bdd
Akshay Gupta <akki199421@gmail.com>
2016-06-03 17:57:17 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,29 +1,25 @@
-var app = angular.module("DealorNoDeal",['ngRoute']);
+var app = angular.module("DealorNoDeal", ['ngAnimate', 'ngRoute']);
 
-app.config(['$routeProvider',function($routeProvider){
-	$routeProvider
-	.when("/",{
-		templateUrl: "partial/home.html",
-		 controller: "homeController"
-		})
-	.when("/game/landing",{
-		templateUrl: "partial/game-landing.html", 
-		controller: "landingController" 
-	})
-	.when("/game/rounds",{
-		templateUrl: "partial/game-rounds.html",
-		controller: "roundController"
-	})
-	.when("/game/bankersOffer",{
-		templateUrl: "partial/bankersOffer.html",
-		controller: "bankerController"
-	})
-	.when("/game/winnings",{
-		templateUrl: "partial/winning.html",
-		controller: "winController"
-	})
-	// .otherwise({
-	// 	redirect:"/"
-	// })
-	
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "partial/home.html",
+            controller: "homeController"
+        })
+        .when("/game/landing", {
+            templateUrl: "partial/game-landing.html",
+            controller: "landingController"
+        })
+        .when("/game/rounds", {
+            templateUrl: "partial/game-rounds.html",
+            controller: "roundController"
+        })
+        .when("/game/bankersOffer", {
+            templateUrl: "partial/bankersOffer.html",
+            controller: "bankerController"
+        })
+        .when("/game/winnings", {
+            templateUrl: "partial/winning.html",
+            controller: "winController"
+        })
 }]);
