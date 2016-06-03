7334fc28f436cdc26a98c5db9142085c354813f7
Akshay Gupta <akki199421@gmail.com>
2016-06-03 17:58:55 +0530

    Update services.js

--- app/services/services.js
+++ app/services/services.js
@@ -1,58 +1,19 @@
 app.factory('gamePlay',function(){
 	
 
-	var srv= {};
-	// to keep track of user details
-	srv.users = {};
+	var Game= {};
+	// to keep track of Plyaer details
+	Game.player = {};
 
 	///money
 	
-	srv.money_list = 
-	{1:false, 
-	2:false, 
-	5:false, 
-	10:false, 
-	25:false, 
-	50:false, 
-	75:false, 
-	100:false, 
-	200:false, 
-	300:false, 
-	400:false, 
-	500:false, 
-	750:false, 
-	1000:false, 
-	5000:false, 
-	10000:false,
-	25000:false, 
-	50000:false, 
-	75000:false, 
-	100000:false, 
-	200000:false, 
-	300000:false, 
-	400000:false, 
-	500000:false, 
-	750000:false, 
-    1000000:false} ;
+	Game.money_list = [1, 2, 5, 10, 25, 50, 75, 100, 200, 300, 400 , 500, 750, 1000, 5000, 10000,
+	25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000 ];
 
-    srv.selectedIndexRight = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
-	srv.selectedIndexLeft = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
-	srv.fetchSelectedIndexLeft = function(){
-		return srv.selectedIndexLeft;
-	}
-	srv.fetchSelectedIndexRight = function(){
-		return srv.selectedIndexRight;
-	}
+    Game.selectedIndexRight = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+	Game.selectedIndexLeft = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
 
-	// [1, 2, 5, 10, 25, 50, 75, 100, 200, 300, 400 , 500, 750, 1000, 5000, 10000,
-	// 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000 ];
-
-	srv.fetchMoney = function(){
-		return angular.copy(Object.keys(srv.money_list));
-	}
-
-	//rounds
-	srv.rounds = [{round:1,turn:6},
+	Game.rounds = [{round:1,turn:6},
 	{round:2,turn:5},
 	{round:3,turn:4},
 	{round:4,turn:3},
@@ -61,9 +22,21 @@ app.factory('gamePlay',function(){
 	{round:7,turn:1},
 	{round:8,turn:1},
 	{round:9,turn:1}];
-	srv.currentRound = 0;
-	srv.turnsLeftInCurrentRound = 0;
-	srv.boxes = [{id:1,"selected":false},
+
+
+	Game.currentRound = 0;
+	Game.turnsLeftInCurrentRound = 0;
+	Game.boxes = [];
+
+	Game.finalWinnings = 0;
+	Game.totalPot = 0;
+	//to keep track of shuffled money
+	Game.moneyRandom = [];
+
+	init = function(){
+		Game.player.playerBox = null;
+		Game.validGame = false;
+		Game.boxes = [{id:1,"selected":false},
 	{id:2,"selected":false},
 	{id:3,"selected":false},
 	{id:4,"selected":false},
@@ -89,122 +62,125 @@ app.factory('gamePlay',function(){
 	{id:24,"selected":false},
 	{id:25,"selected":false},
 	{id:26,"selected":false}];
+	};
 
-	srv.finalWinnings = 0;
-	//set winnings
-	srv.setWinnings = function(amount){
-		srv.finalWinnings = amount;
+	//Set valid Game as true
+	Game.startNewGame = function(playerName){
+		Game.validGame = true;
+		//add player name
+		Game.player.name = playerName;
+		//Shuffle The Money
+		shuffleMoney();
 	}
 
-	srv.getWinnings = function(){
-		return srv.finalWinnings;
-	}
-	//banker's offer
-	srv.bankersOffer = function(){
-		var balance = 3418418 - srv.totalPot;
-		console.log("balance " + srv.totalpot);
-		return Math.floor(Math.round(balance*(srv.currentRound+1)/100));
-	}
-
-	srv.fetchMoneyBoxIndex = function(box){
-		var value = srv.fetchBoxValue(box);
-		var keys = Object.keys(srv.money_list);
-		var m = keys.length;
-		while(m){
-			keys[m] = parseInt(keys[m]);
-			m--;
-		}
-		var index = keys.indexOf(value);
-
-		if(index>=13)
-			srv.selectedIndexRight[index-13]=index-13;
-		else
-			srv.selectedIndexLeft[index]=index;
-		return index;
-	}
-
-	//control rounds
-	srv.startNextRound = function(){
-		//increase current round value
-		console.log('startNextRound');
-		srv.currentRound++;
-		srv.turnsLeftInCurrentRound = srv.rounds[srv.currentRound-1].turn;
-	}
-	srv.fetchTurnsLeft = function(){
-		return srv.turnsLeftInCurrentRound;
+	//get Player Name
+	Game.getPlayerName = function(){
+		return Game.player.name;
 	}
 
 	//fetch valid boxes
-	srv.fetchValidboxes = function(){
+	Game.fetchValidBoxes = function(){
 		var valid= [];
-		angular.forEach(srv.boxes,function(box){
+		angular.forEach(Game.boxes,function(box){
 			if(!box.selected)
 				valid.push(box.id);
 		})
 		return valid;
 	}
 
-	srv.totalPot = 0;
-	srv.moneyRandom = [];
+	//set Player Initial Box 
+	Game.setPlayerBox = function(box){
+		Game.player.playerBox = box;
+		Game.boxes[box-1].selected = true;
+	}
 
-	//player's box
-	init = function(){
-		srv.playerBox = null;
-		srv.validGame = false;
-	};
+	//return Player Box
+	Game.getPlayerBox = function(){
+		return Game.player.playerBox;
+	}
 
-	//Set valid Game as true
-	srv.startNewGame = function(){
-		srv.validGame = true;
+	Game.fetchSelectedIndexLeft = function(){
+		return Game.selectedIndexLeft;
+	}
+	Game.fetchSelectedIndexRight = function(){
+		return Game.selectedIndexRight;
+	}
+
+	//control rounds
+	Game.startNextRound = function(){
+		//increase current round value
+		console.log('startNextRound');
+		Game.currentRound++;
+		Game.turnsLeftInCurrentRound = Game.rounds[Game.currentRound-1].turn;
+	}
+	
+	//return Current Round Value
+	Game.getCurrentRound = function(){
+		return Game.currentRound;
+	}
+
+	//return Turns Left in Current Round
+	Game.fetchTurnsLeft = function(){
+		return Game.turnsLeftInCurrentRound;
+	}
+
+	Game.fetchMoney = function(){
+		return angular.copy(Game.money_list);
+	}
+	
+	//set winnings
+	Game.setWinnings = function(amount){
+		Game.finalWinnings = amount;
+		console.log("Service"+ Game.finalWinnings);
+	}
+
+	Game.getWinnings = function(){
+		return Game.finalWinnings;
+	}
+	//banker's offer
+	Game.bankersOffer = function(){
+		var balance = 3418418 - Game.totalPot;
+		console.log("balance ", Game.totalpot, balance);
+		return Math.floor(Math.round(balance*(Game.currentRound+1)/100));
+	}
+
+	Game.fetchMoneyBoxIndex = function(box){
+		var value = Game.fetchBoxValue(box);
+		var index = Game.money_list.indexOf(value);
+		if(index>=13)
+			Game.selectedIndexRight[index-13]=index-13;
+		else
+			Game.selectedIndexLeft[index]=index;
+		return index;
 	}
 
 	//return valid Game value
-	srv.isValidGame = function(){
-		if(!srv.validGame)
+	Game.isValidGame = function(){
+		if(!Game.validGame)
 			window.location.href = "#/";
 	}
 
 	//new box choosen
-	srv.newBoxChosen = function(box){
-		srv.totalPot += parseInt(srv.moneyRandom[box-1]);
-		console.log("totalpot"+typeof srv.totalPot);
-		// srv.money_list[]
-		srv.boxes[box-1].selected = true;
-		srv.turnsLeftInCurrentRound--;
+	Game.newBoxChosen = function(box){
+		Game.totalPot += Game.moneyRandom[box-1];
+		console.log("totalpot"+typeof Game.totalPot);
+		// Game.money_list[]
+		Game.boxes[box-1].selected = true;
+		Game.turnsLeftInCurrentRound--;
 	}
 
 	//fetch box value
-	srv.fetchBoxValue = function(box){
-		return srv.moneyRandom[box-1];
-	}
-
-	//add Player name
-	srv.addPlayer = function(playerName){
-		srv.users.name = playerName;
-		console.log(srv);
-	}
-
-	//set Player Initial Box 
-	srv.setPlayerBox = function(box){
-		srv.playerBox = box;
-		srv.boxes[box-1].selected = true;
+	Game.fetchBoxValue = function(box){
+		return Game.moneyRandom[box-1];
 	}
 
 	//shuffle money
-	srv.shuffleMoney = function(){
-		srv.moneyRandom = srv.shuffleArray(angular.copy(Object.keys(srv.money_list)));
+	var shuffleMoney = function(){
+		Game.moneyRandom = Game.shuffleArray(angular.copy(Game.money_list));
 	}
 	//Fisher–Yates Shuffle
-	srv.shuffleArray = function(array){
+	Game.shuffleArray = function(array){
 		var m = array.length, t, i;
-	console.log("length = "+m);
-		while(m){
-			array[m] = parseInt(array[m]);
-			m--;
-		}
-		m=array.length;
-		//do not remove this
-		m--;
   		// While there remain elements to shuffle…
   		while (m) {
     	// Pick a remaining element…
@@ -218,5 +194,5 @@ app.factory('gamePlay',function(){
   		return array;
 	}
 	init();
-	return srv;
+	return Game;
 });
